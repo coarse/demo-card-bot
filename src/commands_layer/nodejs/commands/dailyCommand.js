47fc4544f8c77a610f02308e02cbde90ee269dc2
coarse <mfmayol@up.edu.ph>
2023-09-03 23:52:41 +0800

    Adjust dynamodb code

--- src/commands_layer/nodejs/commands/dailyCommand.js
+++ src/commands_layer/nodejs/commands/dailyCommand.js
@@ -1,32 +1,44 @@
 const { SlashCommand } = require("slash-create");
-const { GetCommand } = require("@aws-sdk/lib-dynamodb");
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, GetCommand } = require("@aws-sdk/lib-dynamodb");
 
-const { getDocClient } = require("../clients/dynamodb");
+const client = new DynamoDBClient();
+const docClient = DynamoDBDocumentClient.from(client);
 
 module.exports = class DailyCommand extends SlashCommand {
   constructor(creator) {
     super(creator, {
       name: "daily",
-      description: "Use this command to claim your daily reward.",
+      description: "Use this command to claim your daily rewards.",
       guildIDs: ["1143840924472979559"],
     });
     this.filePath = __filename;
   }
 
   async run(ctx) {
+    console.log('context', ctx)
     const { id: userId } = ctx.user;
 
     // Fetch user from players table
-    const docClient = getDocClient();
     const input = {
       TableName: "dcb-players",
       Key: {
         id: userId,
       },
     };
-    const command = new GetCommand(input);
-    const response = await docClient.send(command);
-    console.log(response);
+    const command = new GetCommand({
+        TableName: 'dcb-players',
+        Key: {
+            id: userId,
+        }
+    });
+
+    try {
+        const response = await docClient.send(command);
+        console.log(response);
+    } catch (error) {
+        console.log('Failed to fetch from table', error);
+    }
 
     return `Claiming reward for ${userId}...`;
   }
